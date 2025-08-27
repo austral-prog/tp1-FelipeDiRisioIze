ea59d55f97fa3164e95a6b6c05d602bb7ad8096e
FelipeDiRisioIze <fdirisioize@mail.austral.edu.ar>
2025-08-27 05:50:52 -0700

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,19 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    diferencia = a - b
+    producto = a * b
+    promedio = (a + b) / 2
+    cociente = a // b
+    resto = a % b
+    division = a / b
+
+    print(suma)
+    print(diferencia)
+    print(producto)
+    print(promedio)
+    print(cociente)
+    print(resto)
+    print(division) 
+
